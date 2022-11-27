76854a54ad85c8e53d72f91eaf5b364a620287ca
vitalspace <myglobalsounds@gmail.com>
2022-11-27 16:19:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nativescript-stackblitz-templates-pjugns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nativescript-stackblitz-templates-17nwnx)
---
+++ app/components/Home.svelte
@@ -0,0 +1,16 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <label class="text-xl align-middle text-center text-gray-500" text="{message}" />
+    </gridLayout>
+</page>
+
+<script lang="ts">
+    let message: string = "Blank Svelte Native App"
+</script>
+
+<style>
+    /* .info {
+        font-size: 20;
+    } */
+</style>
